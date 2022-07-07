8342e0495a06f2afdf983503b15a0360344e11fe
Jacson R. Sasse <jacson.sasse@hotmail.com>
2022-07-07 00:22:42 -0300

    Finished the navbar component layout

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,8 +3,15 @@ import { NavbarDesign } from './style';
 export const Navbar = () => {
     return (
         <NavbarDesign>
-            <div>X</div>
-            <div>º</div>
+            <div id="burguer-section">
+                <input type="checkbox" id="burguer" />
+                <label htmlFor="burguer"></label>
+            </div>
+            <div id="toggle-section">
+                <input type="checkbox" id="toggle" />
+                <label htmlFor="toggle"></label>
+                <span className="ball"></span>
+            </div>
         </NavbarDesign>
     );
 };
--- src/components/Navbar/style.ts
+++ src/components/Navbar/style.ts
@@ -1,11 +1,93 @@
 import styled from 'styled-components';
 
 export const NavbarDesign = styled.nav`
-    background-color: #dbdbdb;
+    background-color: #15191d;
     width: 100vw;
     height: 100%;
     padding: 0 2.75rem;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    user-select: none;
+
+    & #burguer-section {
+        & #burguer {
+            display: none;
+        }
+
+        & label,
+        label::after,
+        label::before {
+            position: absolute;
+            width: 2rem;
+            height: 0.3rem;
+            background: #babbbc;
+            transition: 0.6s;
+        }
+
+        & label {
+            cursor: pointer;
+
+            &::before {
+                content: '';
+                transform: translateY(-0.75rem);
+            }
+
+            &::after {
+                content: '';
+                transform: translateY(0.75rem);
+            }
+        }
+
+        & #burguer:checked {
+            & + label {
+                width: 0px;
+
+                &::before {
+                    transform: rotate(45deg) translate(0px);
+                }
+
+                &::after {
+                    transform: rotate(-45deg) translate(0px);
+                }
+            }
+        }
+    }
+
+    & #toggle-section {
+        position: relative;
+
+        & #toggle {
+            display: none;
+
+            &:checked ~ span.ball {
+                transform: translate(24px, -50%);
+            }
+
+            & + label {
+                display: block;
+                width: 3.125rem;
+                height: 1.5rem;
+                border: 1px solid #babbbc;
+                border-radius: 30px;
+                background-color: #babbbc;
+                user-select: none;
+                cursor: pointer;
+            }
+        }
+
+        & .ball {
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            display: inline-block;
+            width: 15px;
+            height: 15px;
+            background-color: black;
+            border-radius: 50%;
+            left: 5px;
+            z-index: 10;
+            transition: transform 0.3s ease-in-out;
+        }
+    }
 `;
